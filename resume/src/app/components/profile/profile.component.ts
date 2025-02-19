36bed9b10a9e9dbef8c60a1618b90535146f0b4c
Howie Lynch <thegrinch1985@gmail.com>
2025-02-19 20:12:48 +0000

    Styling

--- resume/src/app/components/profile/profile.component.ts
+++ resume/src/app/components/profile/profile.component.ts
@@ -15,7 +15,7 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
     // Start the typing effect
-    this.startTypingEffect();
+    // this.startTypingEffect();
 
     // Navigate to the resume page after 5 seconds
     setTimeout(() => {
