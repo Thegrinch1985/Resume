e491af7be9e28d22755e99118b686266797143ff
Howie Lynch <thegrinch1985@gmail.com>
2025-02-28 15:09:08 +0000

    More Styling

--- resume/src/app/components/navigation/navigation.component.ts
+++ resume/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
@@ -7,21 +7,45 @@ import { ThemeService } from 'src/app/services/theme.service';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-  isLightMode: boolean = true; // Default to light mode
+  isLightMode: boolean = true;
+  menuOpen: boolean = false;
 
-  constructor(private themeService: ThemeService) {}
+  constructor(private themeService: ThemeService, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.isLightMode = this.themeService.getSystemTheme() === 'light';
+    this.applyTheme();
   }
 
-  // Toggle between light and dark mode
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (this.menuOpen) {
+      const clickedInside = (event.target as HTMLElement).closest('.nav-menu, .hamburger');
+      if (!clickedInside) {
+        this.closeMenu();
+      }
+    }
+  }
+
   toggleDarkMode() {
     this.isLightMode = !this.isLightMode;
     this.themeService.setDarkMode(!this.isLightMode);
+    this.applyTheme();
   }
 
-  // Use a getter for dynamic button text
+  applyTheme() {
+    const theme = this.isLightMode ? 'light-theme' : 'dark-theme';
+    this.renderer.setAttribute(document.body, 'class', theme);
+  }
+
   get buttonText(): string {
     return this.isLightMode ? '🌙' : '🌞';
   }
